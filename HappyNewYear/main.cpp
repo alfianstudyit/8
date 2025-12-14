70d88840bdf52caa99d4b91204cd8dfad199dedd
alfianchandra2607 <alfianchandra783@gmail.com>
2025-12-14 15:25:11 +0700

    update

--- HappyNewYear/main.cpp
+++ HappyNewYear/main.cpp
@@ -1,20 +1,61 @@
 #include <iostream>
-#include <string>
+#include <Windows.h>
 using namespace std;
 
 int main() {
 
-	char duaribu[5][5] = {
-		{char(178),char(178),char(178),char(178),char(178)},
-		{' ',' ',' ',' ',char(178)},
-		{char(178),char(178),char(178),char(178),char(178)},
-		{char(178),' ',' ',' ',' '},
-		{char(178),char(178),char(178),char(178),char(178)}
-	};
+    char year[4][5][5] = {
+        // Angka 2
+        {
+            {char(178), char(178), char(178), char(178), char(178)},
+            {' ', ' ', ' ', ' ', char(178)},
+            {char(178), char(178), char(178), char(178), char(178)},
+            {char(178), ' ', ' ', ' ', ' '},
+            {char(178), char(178), char(178), char(178), char(178)}
+        },
+        // Angka 0
+        {
+            {char(178), char(178), char(178), char(178), char(178)},
+            {char(178), ' ', ' ', ' ', char(178)},
+            {char(178), ' ', ' ', ' ', char(178)},
+            {char(178), ' ', ' ', ' ', char(178)},
+            {char(178), char(178), char(178), char(178), char(178)}
+        },
+        // Angka 2
+        {
+            {char(178), char(178), char(178), char(178), char(178)},
+            {' ', ' ', ' ', ' ', char(178)},
+            {char(178), char(178), char(178), char(178), char(178)},
+            {char(178), ' ', ' ', ' ', ' '},
+            {char(178), char(178), char(178), char(178), char(178)}
+        },
+        // Angka 6
+        {
+            {char(178), char(178), char(178), char(178), char(178)},
+            {char(178), ' ', ' ', ' ', ' '},
+            {char(178), char(178), char(178), char(178), char(178)},
+            {char(178), ' ', ' ', ' ', char(178)},
+            {char(178), char(178), char(178), char(178), char(178)}
+        }
+    };
 
-	
-	
+    char greeting[] = "Happy New Year";
 
-	cout << duaribu[0];
-	return 0;
+    for (int i = 0;greeting[i] != '\0';++i) {
+        cout << greeting[i] << flush;
+        Sleep(250);
+    }
+
+    cout << endl;
+    for (int i = 0; i < 5; i++) {
+        for (int o = 0; o < 4; o++) {
+            for (int j = 0; j < 5; j++) {
+                cout << year[o][i][j];
+            }
+            cout << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
 }
